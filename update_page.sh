1eef13ba935306893eccffec8e4db89b36098d67
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-09-05 14:36:16 -0400

    create script for updating existing page

---
+++ update_page.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+# This should export the requested jupyter notebook to HTML and add the html file to the pages branch,
+# for the purpose of github pages
+
+HTML=$1.html
+JNB=$1.ipynb
+
+if ! [[ -f $HTML ]]
+then
+        echo "File $HTML does not exist; just a warning."
+fi
+
+rm $HTML ~/.trash
+git checkout master $JNB
+jupyter nbconvert --execute --to html $JNB
+rm $JNB
+
+echo "Now you should do the following:"
+echo "git add $HTML"
+echo "vim index.md"
+echo "git commit -a"
+echo "git push --all origin"
+echo "git checkout master"
